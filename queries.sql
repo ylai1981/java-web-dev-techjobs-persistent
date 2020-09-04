261bc5fe08b26bf939d8edf0745ae630b9dd720d
Jilar Beltran <jilarb@icloud.com>
2020-09-03 22:06:32 -0400

    final code

--- queries.sql
+++ queries.sql
@@ -1,7 +1,22 @@
 ## Part 1: Test it with SQL
+SHOW COLUMNS FROM job;
+
+
 
 ## Part 2: Test it with SQL
 
+SELECT employer.name
+FROM employer
+WHERE (location = "St. Louis City");
+
+
 ## Part 3: Test it with SQL
 
+DROP TABLE job;
+
 ## Part 4: Test it with SQL
+
+SELECT DISTINCT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name;
